f41d87afdd1c7104b7850429c2c2e0ca53e7b1a3
Martina Burjanova <martina.burjanova@konicaminolta.cz>
2020-06-20 13:19:06 +0200

    osetreny oblasti bez skolek

--- flask_app.py
+++ flask_app.py
@@ -31,25 +31,11 @@ def map():
 def skolky():
     hledanemisto=request.form["misto"]
 
-    def haversine(coord1, coord2):
-        R = 6372800  # Earth radius in meters
-        lat1, lon1 = coord1
-        lat2, lon2 = coord2
-
-        phi1, phi2 = math.radians(lat1), math.radians(lat2)
-        dphi       = math.radians(lat2 - lat1)
-        dlambda    = math.radians(lon2 - lon1)
-
-        a = math.sin(dphi/2)**2 + \
-            math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)**2
-
-        return 2*R*math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
+    """geolocator.geocode() je funkce, která vrátí souřadnice hledaného místa:"""
     geolocator = Nominatim(user_agent="mooi@email.cz")
     location = geolocator.geocode(hledanemisto)
     lat1 = location.latitude
     lon1 = location.longitude
-
     #print(lat1, lon1)
 
     poradi_vzdalenosti = []
@@ -61,16 +47,34 @@ def skolky():
     for row in rows:
         lat2 = float(row[4])
         lon2 = float(row[5])
+        """vybere pouze ty školky, které jsou ve čtverci 10x10km kolem zadanéh místa:"""
         if ( lat2 < (lat1+0.065)) and (lat2 > (lat1-0.065)) and (lon2 > (lon1-0.163)) and (lon2 < (lon1+0.163)):
-        	coord1 = lat1, lon1
-        	coord2 = lat2, lon2
-        	vzdalenost = (haversine(coord1, coord2))/1000
-        	poradi_vzdalenosti.append([row[1], row[2], row[3], float('%2.2f' % (vzdalenost)), row[4], row[5]])
+            coord1 = lat1, lon1
+            coord2 = lat2, lon2
+            vzdalenost = (haversine(coord1, coord2))/1000
+            poradi_vzdalenosti.append([row[1], row[2], row[3], float('%2.2f' % (vzdalenost)), row[4], row[5]])
 
-    poradi = sorted(poradi_vzdalenosti, key=lambda x: x[3])
+    if len(poradi_vzdalenosti) == 0:
+        return render_template("zadne_skolky.html", title="Žádné školky v oblasti", misto=hledanemisto)
+    else:
+        poradi = sorted(poradi_vzdalenosti, key=lambda x: x[3])
+        return render_template("skolky.html", title = "Nalezené školky", misto=hledanemisto, vysledne_poradi=poradi)
 
-    return render_template("skolky.html", title = "Nalezené školky", misto=hledanemisto, vysledne_poradi=poradi)
 
+def haversine(coord1, coord2):
+    """funkce vypočítá vzdálenost mezi dvěma body na zemi z GPS souřadnic"""
+    R = 6372800  # Earth radius in meters
+    lat1, lon1 = coord1
+    lat2, lon2 = coord2
+
+    phi1, phi2 = math.radians(lat1), math.radians(lat2)
+    dphi       = math.radians(lat2 - lat1)
+    dlambda    = math.radians(lon2 - lon1)
+
+    a = math.sin(dphi/2)**2 + \
+        math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)**2
+
+    return 2*R*math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
 
 """
